75c17387dd3059151289f0be08201f68a09a7379
efrot <romanefrot@gmail.com>
2023-09-12 18:36:55 +0200

    generate calculation way, install parse npm package

--- src/Collatz/CollatzInfo.jsx
+++ src/Collatz/CollatzInfo.jsx
@@ -1,12 +1,29 @@
 import './Collatz.css';
+import parse from 'html-react-parser';
 
 const CollatzInfo = (props) => {
     let steps = props.steps;
+    let xValues = props.xValues;
+
+    console.log(xValues);
+
+    const generateCalculationWay = (xValues) => {
+        let caluclationWay = "";
+        xValues.forEach((xValue, index) => {
+            caluclationWay += `<h3>Step ${index+1}</h3>`;
+            if (xValue %2 == 0) {
+                caluclationWay += `<p>The number <b>${xValue}</b> is even, divide it by 2.</p>`
+            } else {
+                caluclationWay += `<p>The number <b>${xValue}</b> is uneven, multiply by 3 and add 1.</p>`
+            }
+        });
+        return caluclationWay;
+    } 
 
     return (
         <div className="collatz-info-container">
-            <h2>Calculation</h2>
-            <p>Steps: {steps}</p>
+            <h2 className="calculation-title">Calculation</h2>
+            {parse(generateCalculationWay(xValues))}
         </div>
     )
 
